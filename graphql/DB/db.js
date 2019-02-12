006a2065d2d08b4a923326a511bffab1894985fc
hy_sean <comet5485@gmail.com>
2019-02-12 23:34:10 +0900

    add movie api

--- graphql/DB/db.js
+++ graphql/DB/db.js
@@ -1,90 +1,16 @@
-export let movies = [
-    {
-        id: 1,
-        title: "The Upside",
-        director: "Neil Burger",
-        opening: "January 11, 2019"
-    },
-    {
-        id: 2,
-        title: "Glass",
-        director: "M. Night Shyamalan",
-        opening: "January 18, 2019"
-    },
-    {
-        id: 3,
-        title: "Serenity",
-        director: "Steven Knight",
-        opening: "January 25, 2019"
-    },
-    {
-        id: 4,
-        title: "What Men Want",
-        director: "Adam Shankman",
-        opening: "February 8, 2019"
-    },
-    {
-        id: 5,
-        title: "The LEGO Movie 2: The Second Part",
-        director: "Mike Mitchell, Trisha Gum",
-        opening: "February 8, 2019"
-    },
-    {
-        id: 6,
-        title: "Alita: Battle Angel",
-        director: "Robert Rodriguez",
-        opening: "February 14, 2019"
-    },
-    {
-        id: 7,
-        title: "How to Train Your Dragon: The Hidden World",
-        director: "Dean DeBlois",
-        opening: "February 22, 2019"
-    },
-    {
-        id: 8,
-        title: "Captain Marvel",
-        director: "Anna Boden, Ryan Fleck",
-        opening: "March 8, 2019"
-    },
-    {
-        id: 9,
-        title: "Under the Silver Lake",
-        director: "David Robert Mitchell",
-        opening: "April 19, 2019"
-    },
-    {
-        id: 10,
-        title: "Avengers: Endgame",
-        director: "Joe Russo, Anthony Russo",
-        opening: "April 26, 2019"
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.am/api/v2/list_movies.json";
+
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if (limit > 0) {
+        REQUEST_URL += `?limit=${limit}`;
     }
-];
-
-export const getMovies = () => movies;
-
-export const getMovieById = id => {
-    const filteredMovie = movies.filter(movie => movie.id === id);
-    return filteredMovie[0];
-}
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if (movies.length > cleanedMovies.length) {
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
+    if (rating > 0) {
+        REQUEST_URL += `&minimum_rating=${rating}`;
     }
+    return fetch(REQUEST_URL)
+        .then(res => res.json())
+        .then(json => json.data.movies);
 }
-
-export const addMovie = (title, director, opening) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        title,
-        director,
-        opening
-    };
-    movies.push(newMovie);
-    return newMovie;
-};
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import { movies, getMovies, getMovieById, deleteMovie, addMovie } from "./DB/db";
+import { getMovies } from "./DB/db";
 
 const resolvers = {
     Query: {
-        movies: () => movies,
-        movie: (_, { id }) =>  getMovieById(id)
-    },
-    Mutation: {
-        addMovie: (_, { title, director, opening }) => addMovie(title, director, opening),
-        deleteMovie: (_, { id }) => deleteMovie(id)
+        movies: (_, { limit, rating }) => getMovies(limit, rating)
     }
 }
 
